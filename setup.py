aa9f14ce7361d869b8f4cbfcd40cbcfa5e45dd1d
richardscholtens <richardscholtens2@gmail.com>
2020-10-21 11:56:48 +0200

    Changed repo url

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/richardscholtens/fibonacci_calculator',
-    version='0.1.0',
+    url='https://github.com/richardscholtens/Fibonacci_Calculator',
+    version='0.1.1',
     zip_safe=False,
 )
